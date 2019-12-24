c8468935d30c4cddc38d59561f047f34f703d91a
AccidentallyOnPurpose <4935931+AccidentallyOnPurpose@users.noreply.github.com4935931+AccidentallyOnPurpose@users.noreply.github.com>
2019-12-24 16:53:43 +0800

    download_model for pytorch

--- download_model.sh
+++ download_model.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env bash
+%%bash
 cd "$(dirname "${0}")"
 BASE_DIR="$(pwd)"
 
 BASE_DIR="$(pwd)"
-MODELS_DIRECTORY=generator/gpt2/models
+MODELS_DIRECTORY=models
 MODEL_VERSION=model_v5
 
 MODEL_DIRECTORY="${MODELS_DIRECTORY}"
 
 MODEL_NAME=pytorch-gpt2-xl-aid2-v5
-MODEL_TORRENT_URL="https://github.com/AccidentallyOnPurpose/pytorch-AIDungeon/blob/f692e39d84/generator/gpt2/models/model_v5_pytorch.torrent?raw=true"
+MODEL_TORRENT_URL="https://raw.githubusercontent.com/AccidentallyOnPurpose/pytorch-AIDungeon/f692e39d84b21d39da9819142165a05a03030892/generator/gpt2/models/model_v5_pytorch.torrent"
 MODEL_TORRENT_BASENAME="$(basename "${MODEL_TORRENT_URL}")"
 
 download_torrent() {
@@ -17,7 +17,6 @@ download_torrent() {
   mkdir -p "${MODEL_DIRECTORY}"
   cd "${MODEL_DIRECTORY}"
   wget "${MODEL_TORRENT_URL}"
-  unzip "${MODEL_TORRENT_BASENAME}"
   which aria2c > /dev/null
   if [ $? == 0 ]; then
     echo -e "\n\n==========================================="
@@ -25,6 +24,9 @@ download_torrent() {
     echo "It will take a while to get up to speed."
     echo "DHT errors are normal."
     echo -e "===========================================\n"
+    echo "$MODEL_TORRENT_BASENAME"
+    echo "${MODEL_TORRENT_BASENAME%.*}"
+    ls
     aria2c \
       --max-connection-per-server 16 \
       --split 64 \
@@ -32,7 +34,7 @@ download_torrent() {
       --seed-time=0 \
       --summary-interval=15 \
       --disable-ipv6 \
-      "${MODEL_TORRENT_BASENAME%.*}"
+      "${MODEL_TORRENT_BASENAME}"
     echo "Download Complete!"
     mv "${MODEL_TORRENT_BASENAME%.*}" $MODEL_NAME
     fi
@@ -45,19 +47,7 @@ redownload () {
 }
 
 if [[ -d "${MODEL_DIRECTORY}" ]]; then
-	ANSWER="n"
-	echo "AIDungeon2 Model appears to be downloaded."
-	echo "Would you like to redownload?"
-	echo "WARNING: This will remove the current model![y/N]"
-	read ANSWER
-	ANSWER=$(echo $ANSWER | tr '[:upper:]' '[:lower:]')
-	case $ANSWER in
-		 [yY][eE][sS]|[yY])
-			redownload;;
-		*)
-			echo "Exiting program!"
-			exit;;
-	esac
+  redownload
 else
 	download_torrent
 fi
